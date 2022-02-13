ffef2b707ced7ecd863ee6390681b34f110706af
sodock00 <76846145+sodock00@users.noreply.github.com>
2022-02-13 19:26:49 +0900

    [Bronze IV] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498.시험 성적/시험 성적.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	switch(a/10) {
+	case 10:
+	case 9:
+		cout << "A";
+		break;
+	case 8:
+		cout << "B";
+		break;
+	case 7:
+		cout << "C";
+		break;
+	case 6:
+		cout << "D";
+		break;
+	default:
+		cout << "F";
+		break;
+	}
+	
+	return 0;
+}
