34b090d9e8912fa39d8bd18f9fd1f5b7ed6abcf9
sodock00 <76846145+sodock00@users.noreply.github.com>
2022-03-13 19:57:14 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int h, m;
+	cin >> h >> m;
+	if (m - 45 < 0) {
+		if(h==0) cout << 23 << " " << m + 15;
+		else cout << h - 1 << " " << m + 15;
+	}
+	else cout << h << " " << m - 45;
+	return 0;
+}
