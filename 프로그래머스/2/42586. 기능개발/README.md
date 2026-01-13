006202683204a36ea0cc96f75447f923b2fbe393
e <76846145+sodock00@users.noreply.github.com>
2026-01-14 07:11:21 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,6 +1,6 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 14일 07:10:56
+2026년 01월 14일 07:11:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,3 @@
-from collections import deque
 def solution(progresses, speeds):
     answer = []
-    # 1. progress가 100이 되기 위해 필요한 day 수를 각 speed를 통해 미리 계산
-    days = [0 for _ in range(len(progresses))]
-    for i in range(len(progresses)):
-        progress = progresses[i]
-        speed = speeds[i]
-        days[i] = (100 - progress + speed - 1) // speed
-    # 2. day를 0 인덱스부터 비교
-    count = 1
-    prev = days[0]
-    for day in days[1:]:
-        # 2-1. 맨 앞 day보다 작은 값 나올 경우 - count += 1
-        if day <= prev:
-            count += 1
-        # 2-2. 맨 앞 day보다 큰 값 나올 결루 - ans에 count 추가하고 count 1 초기화
-        else:
-            answer.append(count)
-            prev = day
-            count = 1
-            
-    answer.append(count)
-        
     return answer
